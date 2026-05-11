57db5d04ab3801e3d67b9e3ab3217d0c04a8e0fa
Lucky Ivanius <luckzivanius@gmail.com>
2026-05-11 23:46:27 +0700

    feat: wallet connection gated swap

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,11 @@ import { useWalletAdapterCompat } from "@solana/connector/compat";
 import { useConnector, useTransactionSigner } from "@solana/connector/react";
 import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 import { TokenInput } from "@/components/token-input";
 import { Button } from "@/components/ui/button";
+import { SolanaWalletConnectDialog } from "@/components/wallet-connection/solana";
 import { useCluster } from "@/hooks/use-cluster";
 import { isToken } from "@/lib/tokens";
 
@@ -29,12 +31,14 @@ function Swap() {
   const { cluster } = useCluster();
   const { from, to } = Route.useSearch();
   const navigate = Route.useNavigate();
-  const { disconnectWallet } = useConnector();
+  const { disconnectWallet, isConnected } = useConnector();
   const { signer } = useTransactionSigner();
   const { signMessage } = useWalletAdapterCompat(signer, disconnectWallet);
 
   const [sellAmount, setSellAmount] = useState<number | null>(null);
   const [buyAmount, setBuyAmount] = useState<number | null>(null);
+
+  const [walletConnectDialogOpen, setWalletConnectDialogOpen] = useState(false);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -57,91 +61,113 @@ function Swap() {
             <HugeiconsIcon icon={Settings01Icon} />
           </Button>*/}
         </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <TokenInput
-            title="Sell"
-            token={isToken(from, cluster) ? from : null}
-            onTokenChange={(token) => {
-              navigate({
-                search: (prev) => {
-                  const next = token ?? undefined;
-                  if (next && next === prev.to)
-                    return { ...prev, from: next, to: prev.from };
+        <div className="flex flex-col w-full items-center justify-center gap-2 relative">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <TokenInput
+              title="Sell"
+              token={isToken(from, cluster) ? from : null}
+              onTokenChange={(token) => {
+                navigate({
+                  search: (prev) => {
+                    const next = token ?? undefined;
+                    if (next && next === prev.to)
+                      return { ...prev, from: next, to: prev.from };
 
-                  return { ...prev, from: next };
-                },
-                replace: true,
-              });
-            }}
-            amount={sellAmount}
-            onAmountChange={setSellAmount}
-            balance={0}
-          />
-          <Button
-            tabIndex={-1}
-            variant="secondary"
-            size="icon"
-            className="group absolute z-10 bg-background hover:bg-background ring-1 ring-muted"
-            onClick={() => {
-              navigate({
-                search: (prev) => ({ from: prev.to, to: prev.from }),
-                replace: true,
-              });
-
-              setBuyAmount(null);
-              setSellAmount(buyAmount);
-            }}
-          >
-            <HugeiconsIcon
-              icon={ChevronDown}
-              className="group-hover:rotate-180 transition-[rotate] duration-200"
+                    return { ...prev, from: next };
+                  },
+                  replace: true,
+                });
+              }}
+              amount={sellAmount}
+              onAmountChange={setSellAmount}
+              balance={0}
             />
-          </Button>
-          <TokenInput
-            readOnly
-            title="Buy"
-            token={isToken(to, cluster) ? to : null}
-            onTokenChange={(token) => {
-              navigate({
-                search: (prev) => {
-                  const next = token ?? undefined;
-                  if (next && next === prev.from)
-                    return { ...prev, from: prev.to, to: next };
+            <Button
+              tabIndex={-1}
+              variant="secondary"
+              size="icon"
+              className="group absolute z-10 bg-background hover:bg-background ring-1 ring-muted"
+              onClick={() => {
+                navigate({
+                  search: (prev) => ({ from: prev.to, to: prev.from }),
+                  replace: true,
+                });
 
-                  return { ...prev, to: next };
-                },
-                replace: true,
-              });
-            }}
-            amount={buyAmount}
-            onAmountChange={setBuyAmount}
-            balance={0}
-          />
-        </div>
-        <div className="flex w-full items-center justify-center gap-2">
-          <Button
-            className="w-full"
-            size="lg"
-            disabled={!from || !to || !sellAmount || !buyAmount}
-            onClick={async () => {
-              if (!signMessage) return;
+                setBuyAmount(null);
+                setSellAmount(buyAmount);
+              }}
+            >
+              <HugeiconsIcon
+                icon={ChevronDown}
+                className="group-hover:rotate-180 transition-[rotate] duration-200"
+              />
+            </Button>
+            <TokenInput
+              readOnly
+              title="Buy"
+              token={isToken(to, cluster) ? to : null}
+              onTokenChange={(token) => {
+                navigate({
+                  search: (prev) => {
+                    const next = token ?? undefined;
+                    if (next && next === prev.from)
+                      return { ...prev, from: prev.to, to: next };
 
-              const signature = await signMessage(
-                new TextEncoder().encode(
-                  JSON.stringify({
-                    from,
-                    to,
-                    sellAmount,
-                    buyAmount,
-                  })
-                )
-              );
+                    return { ...prev, to: next };
+                  },
+                  replace: true,
+                });
+              }}
+              amount={buyAmount}
+              onAmountChange={setBuyAmount}
+              balance={0}
+            />
+          </div>
+          <div className="flex w-full items-center justify-center gap-2">
+            {isConnected ? (
+              <Button
+                className="w-full"
+                size="xl"
+                disabled={!from || !to || !sellAmount || !buyAmount}
+                onClick={async () => {
+                  if (!signMessage)
+                    return toast.warning(
+                      "Wallet not supported for signing messages"
+                    );
 
-              console.log(signature);
-            }}
-          >
-            Swap
-          </Button>
+                  const signature = await signMessage(
+                    new TextEncoder().encode(
+                      JSON.stringify({
+                        from,
+                        to,
+                        sellAmount,
+                        buyAmount,
+                      })
+                    )
+                  );
+
+                  console.log(signature);
+                }}
+              >
+                Swap
+              </Button>
+            ) : (
+              <>
+                <Button
+                  className="w-full"
+                  size="xl"
+                  onClick={() => setWalletConnectDialogOpen(true)}
+                >
+                  Connect Wallet
+                </Button>
+                <SolanaWalletConnectDialog
+                  open={walletConnectDialogOpen}
+                  onOpenChange={setWalletConnectDialogOpen}
+                  onConnect={() => {}}
+                />
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
